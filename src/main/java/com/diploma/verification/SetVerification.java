8c9d919b2c9aad5d0dbd7bf6af82916c23dd86d3
outsidereal <outsidereal@gmail.com>
2013-11-19 17:24:06 +0200

    Set- method created class..

---
+++ src/main/java/com/diploma/verification/SetVerification.java
@@ -0,0 +1,12 @@
+package com.diploma.verification;
+
+/**
+ * User: d.ulanovych
+ * Date: 11/19/13
+ */
+public class SetVerification implements Verification {
+    @Override
+    public boolean verify() {
+        return false;
+    }
+}

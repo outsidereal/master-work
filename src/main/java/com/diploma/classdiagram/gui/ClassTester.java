445882e30221a8de3727cb7819c016613943b91a
outsidereal <outsidereal@gmail.com>
2013-12-11 11:54:19 +0200

    extended GUI, small additions in MASTER_WORK.docx

--- src/main/java/com/diploma/classdiagram/gui/ClassTester.java
+++ src/main/java/com/diploma/classdiagram/gui/ClassTester.java
@@ -1,6 +1,13 @@
 package com.diploma.classdiagram.gui;
 
+import com.diploma.classdiagram.parser.ClassParser;
+import com.diploma.classdiagram.parser.UMLParserException;
+import com.diploma.classdiagram.verification.*;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 /**
  * User: d.ulanovych
@@ -8,11 +15,132 @@ import javax.swing.*;
  */
 public class ClassTester {
     private JPanel panel1;
-    private JList log;
     private JRadioButton graphRadioButton;
     private JRadioButton testDriverRadioButton;
     private JRadioButton setRadioButton;
     private JRadioButton patternsRadioButton;
+    private JRadioButton complexRadioButton;
     private JButton verifyButton;
-    private JButton button2;
+    private JButton fileSelector;
+    private JTextArea log;
+    private File currentDiagram;
+    final JFileChooser fileChooser = new JFileChooser();
+
+    public ClassTester() {
+        testDriverRadioButton.setSelected(true);
+        Verification.LOGGER.addAppender(new GuiAppender(log));
+        testDriverRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                testDriverRadioButton.setSelected(true);
+                graphRadioButton.setSelected(false);
+                setRadioButton.setSelected(false);
+                patternsRadioButton.setSelected(false);
+                complexRadioButton.setSelected(false);
+            }
+        });
+        graphRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                graphRadioButton.setSelected(true);
+                testDriverRadioButton.setSelected(false);
+                setRadioButton.setSelected(false);
+                patternsRadioButton.setSelected(false);
+                complexRadioButton.setSelected(false);
+            }
+        });
+        setRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setRadioButton.setSelected(true);
+                graphRadioButton.setSelected(false);
+                testDriverRadioButton.setSelected(false);
+                patternsRadioButton.setSelected(false);
+                complexRadioButton.setSelected(false);
+            }
+        });
+        patternsRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                patternsRadioButton.setSelected(true);
+                graphRadioButton.setSelected(false);
+                setRadioButton.setSelected(false);
+                testDriverRadioButton.setSelected(false);
+                complexRadioButton.setSelected(false);
+            }
+        });
+        complexRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                complexRadioButton.setSelected(true);
+                graphRadioButton.setSelected(false);
+                setRadioButton.setSelected(false);
+                patternsRadioButton.setSelected(false);
+                testDriverRadioButton.setSelected(false);
+            }
+        });
+        fileSelector.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fileChooser.setCurrentDirectory(currentDiagram);
+                int returnVal = fileChooser.showOpenDialog(null);
+
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    currentDiagram = fileChooser.getSelectedFile();
+                    log.append(currentDiagram.getAbsolutePath() + "\n");
+                }
+            }
+        });
+        verifyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                verify();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("ClassTester");
+        frame.setContentPane(new ClassTester().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void verify() {
+        if (null != currentDiagram) {
+            ClassParser parser = new ClassParser();
+            Verification verification = null;
+            try {
+                parser.parse(currentDiagram.getAbsolutePath());
+            } catch (UMLParserException e) {
+                for (StackTraceElement element : e.getStackTrace()) {
+                    log.append(element.toString());
+                    log.append("\n");
+                }
+                return;
+            }
+            if (testDriverRadioButton.isSelected()) {
+                verification = new DriverVerification(parser.getParsedClasses(), parser.getParsedRelationships());
+            } else if (setRadioButton.isSelected()) {
+                verification = new SetVerification();
+            } else if (patternsRadioButton.isSelected()) {
+                verification = new PatternVerification(parser.getParsedClasses(), parser.getParsedRelationships());
+            } else if (graphRadioButton.isSelected()) {
+                verification = new GraphVerification(parser.getParsedRelationships());
+            }
+            if (null != verification) {
+                verification.verify();
+            } else if (complexRadioButton.isSelected()) {
+                verification = new DriverVerification(parser.getParsedClasses(), parser.getParsedRelationships());
+                verification.verify();
+                verification = new SetVerification();
+                verification.verify();
+                verification = new PatternVerification(parser.getParsedClasses(), parser.getParsedRelationships());
+                verification.verify();
+                verification = new GraphVerification(parser.getParsedRelationships());
+                verification.verify();
+            }
+        }
+    }
 }
